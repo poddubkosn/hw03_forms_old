aa31bc25362587f0af9b437c0d2e98ab9f1926c9
Поддубко Сергей <snpod@rambler.ru>
2021-08-29 23:24:02 +1000

    Социальная сеть.v3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -31,7 +31,7 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
-    author = User.objects.get(username=username)
+    author = get_object_or_404(User, username=username)
     post_list = author.posts.all()
     posts_count = post_list.count()
     paginator = Paginator(post_list, number_of_elements_in_page)
